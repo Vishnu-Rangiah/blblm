387b96c28a5547c45f5b38ec51a8a7f47e9d5944
vishnu rangiah <vrrangiah@ucdavis.edu>
2021-03-17 23:55:47 -0700

    - final 1

--- R/blblm.R
+++ R/blblm.R
@@ -1,22 +1,64 @@
 #' @import purrr
+#' @import furrr
 #' @import stats
+#' @import parallel
 #' @importFrom magrittr %>%
 #' @details
 #' Linear Regression with Little Bag of Bootstraps
 "_PACKAGE"
-
 
 ## quiets concerns of R CMD check re: the .'s that appear in pipelines
 # from https://github.com/jennybc/googlesheets/blob/master/R/googlesheets.R
 utils::globalVariables(c("."))
 
 
-#' @export
-blblm <- function(formula, data, m = 10, B = 5000) {
+#' @title blblm is used to implement the bag of little bootstraps for the linear regression model.
+#' @name blblm
+#' @param formula an object of class formula giving relationship of variables in model
+#'
+#' @param data An optional data frame, list or environment containing variables for the linear model.
+#' @param m An optional integer to specify the number of splits (m) in the data set.
+#' @param B An optional integer for each subsample, sample observations with replacement, repeat for (B) times.
+#' @param parallel logical. If TRUE the linear model with be fitted using parallel computing. Function will follow single process if FALSE.
+#' @param nthreads n optional integer to specify the number of cores used only if parallel is TRUE.
+#'
+#' @examples
+#' fit <- blblm(mpg ~ wt * hp, data = mtcars, m = 3, B = 100)
+#' fit2 <- blblm(mpg ~ wt * hp, data = mtcars, m = 3, B = 100, parallel = TRUE, nthreads = 4)
+#'
+#'
+#' @exportbl
+blblm <- function(formula, data, m = 10, B = 5000, parallel = FALSE, nthreads = detectCores()) {
+
   data_list <- split_data(data, m)
-  estimates <- map(
-    data_list,
-    ~ lm_each_subsample(formula = formula, data = ., n = nrow(data), B = B))
+
+  set.seed(123)
+
+  if(parallel){
+    #suppressWarnings(plan(multiprocess, workers = nthreads))
+    options(future.rng.onMisuse = "ignore")
+    if(nthreads<2){
+      nthreads = 2
+    }
+    else if(nthreads>detectCores()){
+      nthreads = detectCores()
+    }
+    cl <- makeCluster(nthreads) #sets seed to be the same as the single process
+
+    clusterSetRNGStream(cl,123)
+
+    estimates <- future_map(data_list,
+            ~ lm_each_subsample(formula = formula, data = ., n = nrow(data), B = B))
+
+    stopCluster(cl)
+  }
+
+  else{
+    # set seed for testing purposes
+    estimates <- map(
+      data_list,
+      ~ lm_each_subsample(formula = formula, data = ., n = nrow(data), B = B))
+    }
   res <- list(estimates = estimates, formula = formula)
   class(res) <- "blblm"
   invisible(res)
@@ -65,6 +107,16 @@ blbsigma <- function(fit) {
 }
 
 
+#' @title print.blblm is used to print the blblm model
+#' @name print.blblm
+#' @param x represents the blblm model or object to be printed
+#'
+#' @param ... additional arguments to be passed
+#'
+#' @examples
+#' fit <- blblm(mpg ~ wt * hp, data = mtcars, m = 3, B = 100)
+#' print.blblm(fit)
+#'
 #' @export
 #' @method print blblm
 print.blblm <- function(x, ...) {
@@ -73,6 +125,20 @@ print.blblm <- function(x, ...) {
 }
 
 
+#' @title sigma.blblm is used to obtain the residual standard deviation
+#' @name sigma.blblm
+#'
+#' @param object an R object typically from a linear model
+#'
+#' @param confidence logical. If TRUE will output the confidence interval for the residual standard deviation
+#' @param level an alpha level for the confidence interval
+#' @param ... additional arguments to be passed
+#'
+#'@examples
+#' fit <- blblm(mpg ~ wt * hp, data = mtcars, m = 3, B = 100)
+#' sigma(fit)
+#' sigma(fit, confidence = TRUE)
+#'
 #' @export
 #' @method sigma blblm
 sigma.blblm <- function(object, confidence = FALSE, level = 0.95, ...) {
@@ -89,6 +155,16 @@ sigma.blblm <- function(object, confidence = FALSE, level = 0.95, ...) {
   }
 }
 
+#' @title coef.blblm is a function to extract the model coefficients from the blblm model
+#' @name coef.blblm
+#' @param object an object which has relevant coefficients to be extracted
+#'
+#' @param ... additional arguments to be passed
+#'
+#' @examples
+#' fit <- blblm(mpg ~ wt * hp, data = mtcars, m = 3, B = 100)
+#' coef(fit)
+#'
 #' @export
 #' @method coef blblm
 coef.blblm <- function(object, ...) {
@@ -97,8 +173,24 @@ coef.blblm <- function(object, ...) {
 }
 
 
+
+#' @title confint.blblm is a function to compute confidence interval for one or multiple parameters in the blblm  model
+#' @name confint.blblm
+#'
+#' @param object a blblm model
+#'
+#' @param parm the parameters given to calculate the confidence intervals
+#' @param level an alpha level for the confidence interval
+#' @param ... additional arguments to be passed
+#'
+#' @examples
+#' fit <- blblm(mpg ~ wt * hp, data = mtcars, m = 3, B = 100)
+#' confint(fit, c("wt", "hp"))
+#'
 #' @export
 #' @method confint blblm
+#'
+#'
 confint.blblm <- function(object, parm = NULL, level = 0.95, ...) {
   if (is.null(parm)) {
     parm <- attr(terms(object$formula), "term.labels")
@@ -115,6 +207,23 @@ confint.blblm <- function(object, parm = NULL, level = 0.95, ...) {
   out
 }
 
+
+#' @title predict.blblm is a function for predictions from blblm model for new data
+#' @name predict.blblm
+#'
+#' @param object a blblm model
+#'
+#'
+#' @param new_data an data frame, list or env containing new data to be used for the prediction
+#' @param confidence logical. If TRUE gives confidence interval of new prediction on blblm linear model/
+#' @param level an alpha level for the confidence interval
+#' @param ... additional arguments to be passed
+#'
+#' @examples
+#' fit <- blblm(mpg ~ wt * hp, data = mtcars, m = 3, B = 100)
+#' predict(fit, data.frame(wt = c(2.5, 3), hp = c(150, 170)))
+#' predict(fit, data.frame(wt = c(2.5, 3), hp = c(150, 170)), confidence = TRUE)
+#'
 #' @export
 #' @method predict blblm
 predict.blblm <- function(object, new_data, confidence = FALSE, level = 0.95, ...) {
--- tests/testthat/test-fit.R
+++ tests/testthat/test-fit.R
@@ -1,3 +1,65 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
-})
+test_that("blblm package functions tests cases", {
+
+  #Test for Blblm AND time
+
+  bnch = bench::mark(
+
+    fit <- blblm(mpg ~ wt * hp, data = mtcars, m = 3, B = 1000, parallel = TRUE, nthreads = 2), #limited to only 2 cores
+    fit2 <- blblm(mpg ~ wt * hp, data = mtcars, m = 3, B = 1000),
+    check = FALSE
+
+    )
+  #View(bnch)
+  #expect_lte(bnch$total_time[1],bnch$total_time[2])
+
+  #length of coeffs
+  fit_len = length(coef(fit))
+  expect_identical(fit_len,4)
+  fit2_len = length(coef(fit2))
+  expect_identical(fit2_len,4)
+
+  #class of coeffs
+  fit_coef = class(coef(fit))
+  fit2_coef = class(coef(fit2))
+  expect_identical(fit_coef,fit2_coef)
+
+  #a represents (fit) a single process and b is the (fit2) parallel process
+  #we should expect the parallel process to have a quicker total run time for larger datasets
+  #however since we have a smaller data-set the initiation time for parallel computing may cause fit to have a longer time than fit2
+
+  #We will similarly test the length of the other exported functions in our package to find any potential errors.
+
+
+  #length of confint
+  cfl = length(confint(fit, c("wt", "hp")))
+  expect_identical(cfl,4)
+  cfl2 = length(confint(fit2, c("wt", "hp"))) #4
+  expect_identical(cfl2,4)
+
+  #length of sigma
+  sl = length(sigma(fit))
+  expect_identical(sl,1)
+  sl2 = length(sigma(fit2)) #1
+  expect_identical(sl2,1)
+
+  #length of sigma with confidence
+  scl = length(sigma(fit, confidence = TRUE)) #3
+  expect_identical(scl,3)
+  scl2 = length(sigma(fit2, confidence = TRUE))
+  expect_identical(scl2,3)
+
+  #length of sigma with confidence
+  pl = length(predict(fit, data.frame(wt = c(2.5, 3), hp = c(150, 170)))) #2
+  expect_identical(pl,2)
+  pl2 = length(predict(fit, data.frame(wt = c(2.5, 3), hp = c(150, 170)))) #2
+  expect_identical(pl2,2)
+
+  #length of sigma with confidence
+  pcl = length(predict(fit, data.frame(wt = c(2.5, 3), hp = c(150, 170)), confidence = TRUE))#6
+  expect_identical(pcl,6)
+  pcl2 = length(predict(fit, data.frame(wt = c(2.5, 3), hp = c(150, 170)), confidence = TRUE))#6
+  expect_identical(pcl2,6)
+
+
+
+  })
